51630926f80157289457251ee76424774a91bc20
Musingsole <musingsole@gmail.com>
2019-06-04 14:54:19 -0400

    Working version

--- WiPy/main.py
+++ WiPy/main.py
@@ -1,6 +1,5 @@
 from machine import reset
 from WiPyFunctions import FlashingLight, LED_RED, LED_OFF, simple_connect
-from GoldenFightFi import GoldenFight
 import time
 from sys import print_exception
 
@@ -9,7 +8,7 @@ if __name__ == "__main__":
         FLASHING_LIGHT = FlashingLight(colors=[LED_RED, LED_OFF], ms=250)
 	print("Flashing red light means everything's running!")
     except Exception as e:
-        print("Failed GoldenFight execution")
+        print("Failed execution")
         print_exception(e)
         time.sleep(5)
         reset()
